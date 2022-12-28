1a854c5eae07acbc2d4ba8371c24f2c046a9a901
ozkanberkee <67657051+ozkanberkee@users.noreply.github.com>
2022-12-28 21:27:13 +0300

    Kod düzeltildi

---
+++ src/Cizelgeleyici/ProsesListesi.java
@@ -0,0 +1,50 @@
+package Cizelgeleyici;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class ProsesListesi {
+
+public List<ProsesYonetici> prosesler = new ArrayList<ProsesYonetici>();
+	
+	
+	//Proses listesi oluşurken dosyadan okuma işlemi yapıp bu bilgiler eşliğinde prosesleri oluşturup
+	//Proses listesine atar.
+public ProsesListesi() throws IOException{
+	int id = 0;
+	File file =new File("giriş.txt");	//Dosya oluşturma
+	
+	FileReader freader = new FileReader(file);	//Dosyayı okuma
+	String line;
+	BufferedReader bReader = new BufferedReader(freader);
+	
+	while ((line = bReader.readLine())!= null) {	//Tüm satırları okuma
+		String[] dizi= line.split(", ");	//Vırgül ve boşlukların dışındaki karakterler çekildi
+		
+		for (int i=0;i<dizi.length;i++) {
+			ProsesYonetici p = new ProsesYonetici();//Proses oluşturuldu.
+			p.id = id++;	//Proseslere 0'dan başlayıp id ataması yapar.
+			p.varisZamani = Integer.parseInt(dizi[i]);
+			p.oncelik = Integer.parseInt(dizi[++i]);
+			p.patlamaZamani = Integer.parseInt(dizi[++i]);
+			
+			prosesler.add(p);
+			
+		}
+		
+	}
+	bReader.close();
+	
+}
+	
+}
---
+++ src/Cizelgeleyici/main.java
@@ -0,0 +1,26 @@
+package Cizelgeleyici;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Iterator;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class main {
+	
+	static int saniye=-1;
+	static ProsesYonetici p = new ProsesYonetici();
+	
+	//Ana Program.
+	public static void main(String[] args) throws IOException {
+		Gorevlendirici gorevlendirici = new Gorevlendirici();
+		gorevlendirici.calistir(p);
+	}
+
+}
